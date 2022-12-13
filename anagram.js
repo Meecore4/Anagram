1a64ed37cff200611d4083feb567375753ce20ff
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2022-12-13 11:36:56 +0100

    enregistrer

---
+++ anagram.js
@@ -0,0 +1,23 @@
+const anagram = (str1, str2) => {
+    str1= str1.toLowerCase();
+    str2= str2.toLowerCase();
+    
+  
+    let strChar1= str1.split('').filter(el => el !== ' ').sort();
+   
+    let strChar2= str2.split('').filter(el => el !== ' ').sort();
+  
+    const result1 = strChar1.join('');
+    const result2 = strChar2.join('');
+
+  
+    
+if (result1 === result2){
+      console.log('true');   
+    } else {
+      console.log('false');   }
+
+  
+};
+anagram('Hello','Hello');
+module.exports = anagram;
